44ebbb0afde5255c63b0d5ed0613c9938d49a7db
Alex <alexliang315@gmail.com>
2025-06-08 15:11:24 +1000

    updated tests

--- tests.py
+++ tests.py
@@ -3,6 +3,8 @@ import sys
 from importlib import reload
 from flask import Flask, render_template, redirect, request, url_for
 
+#Security Tests
+
 def pathtraversal(path):
 #Test that attempts of path traversal will cause an error to be shown instead of displaying the page
 
@@ -14,4 +16,16 @@ def injection():
 
 def token(session):
 #Test that a new token is generated every session
+
+#Functional Tests
+
+def hintdisplay():
+#Test that hints show up on the screen correctly
+
+def themetoggletestcss():
+#Test that the proper CSS is loaded on toggle
+
+def persistence():
+#Test that the theme stays the same across all pages
+
 
